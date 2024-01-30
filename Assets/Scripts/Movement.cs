a84504753e8e5d22e248a8efbf18891eac205de1
Roma Lososev <korsunov.rom@yandex.ru>
2024-01-30 17:34:55 +0300

    Начало

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float dirX, dirZ;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        dirX = Input.GetAxis("Horizontal");
+        dirZ = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(dirX,dirZ);
+    }
+}
