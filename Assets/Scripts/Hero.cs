dd259c6d40f6f97a830fefa63042f6003d07afee
Skrippe3 <97904563+Skrippe3@users.noreply.github.com>
2024-02-02 17:35:43 +0300

    Создал файл Hero и добавил жизни герою

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : Entity
+{
+
+    private void Start()
+    {
+        [SerializeField] lives = 5;
+    }
+
+    private void Update()
+    {
+        
+    }
+}
